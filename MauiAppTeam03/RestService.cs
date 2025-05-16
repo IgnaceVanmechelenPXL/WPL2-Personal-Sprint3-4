042b85ac20b3bec034e6f46a17bdcef4ed8e6eb8
IgnaceVanmechelenPXL <12402669@student.pxl.be>
2025-05-16 09:35:11 +0200

    MAUI-05

---
+++ MauiAppTeam03/RestService.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibTeam03.Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppTeam03
+{
+    public static class RestService
+    {
+        private static readonly HttpClient httpClient = new();
+
+        public static async Task<List<BoulderRoute>> GetBoulderRoutesAsync()
+        {
+            var apiUrl = "https://slrd2bgq-51035.euw.devtunnels.ms/api/BoulderRoutes/all";
+            return await httpClient.GetFromJsonAsync<List<BoulderRoute>>(apiUrl);
+        }
+    }
+}
